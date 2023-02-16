60822e1f53c31fc9aa50f4a39d3dda5049739fb7
EDONOBI-EZINNE <cubeeus@yahoo.com>
2023-02-16 03:40:22 -0800

    all files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+*main - Entry point
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+puts("/"programming is like building a multilinqual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+*main - Entry point
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+         printf("with proper grammr, but the outcome is a piece of art,\n");
+         return (0);
+}
