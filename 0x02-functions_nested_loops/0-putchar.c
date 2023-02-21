5b12d120db0710e930fb3339b797155b249e0892
EDONOBI-EZINNE <cubeeus@yahoo.com>
2023-02-21 04:42:09 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - print out _putchar using printf
  *
- * Return: nothing, return void
+ * Return: 0
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
-return (0);
+	return (0);
 }
