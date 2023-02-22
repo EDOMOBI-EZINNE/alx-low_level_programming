4235923a1b31b09f0014aa4324e58073404e04ea
EDONOBI-EZINNE <cubeeus@yahoo.com>
2023-02-21 23:59:33 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,9 +11,11 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
 		return (1);
-	return (0);
+	}
+	else
+		return (0);
 }
 
