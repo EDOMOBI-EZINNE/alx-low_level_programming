93bdd71d24be919bf978deb4898a0a99fe39391e
root <cubeeus@yahoo.com>
2023-08-08 05:39:59 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,39 @@
 #include "main.h"
-
-#include <stdio.h>
 #include <stdlib.h>
 
-/*******************START*******************/
 /**
- * read_textfile - functions; reads text_file & prints to_stdout.
- * @filename:pointer: shows to the name of the file.
- * @letters: Shows the No. of letters the
- *           function (read_textfile) should read & print.
+ * read_textfile - reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: pointer to the name of the file
+ * @letters: number of letters to read and print
  *
- * Return:Returns 0 if filename is NULL.
- *
- *
- * ALX PROJECTS
+ * Return: 0 if filename is NULL or fn fails
+ * the actual number of letters it could read and print
  */
-
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/****Declarations******/
-	int f_d;
-	ssize_t w_cnt;
-	ssize_t w_r;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	f_d = open(filename, O_RDONLY);
-	if (f_d == -1)
-		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
-	w_r = read(f_d, buffer, letters);
-	if (w_r == -1)
-		return (0);
-	w_cnt = write(STDOUT_FILENO, buffer, w_r);
-	if (w_cnt == -1 || w_r != w_cnt)
-		return (0);
 
 	free(buffer);
-	close(f_d);
-	return (w_cnt);
+	close(o);
+
+	return (w);
 }
-/**************************STOP******************************/
