77637c3ac9852634a872fa48b36958a4bbfdb775
root <zinnyfingers17@gmail.com>
2023-09-28 11:24:55 -0700

    completed

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,8 +11,12 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned long int xor = n ^ m, bits = 0;
+
 while (xor > 0)
 {
 bits += (xor & 1);
 xor >>= 1;
+}
+
+return (bits);
 }
